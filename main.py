32b6c71b58261ec7b06e2d0138c758d690eac59f
fj7 <85462986+febyjose12@users.noreply.github.com>
2021-10-19 13:17:01 -0400

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ content of calculator.py#"""
 
 def inc(x_value):
-    """ Increment F unction adds one to the x_value"""
+    """ Increment F unction adds on e to the x_value"""
     return x_value + 1
 def test_answer():
     """This Tests the function"""
